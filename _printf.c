2e2b8e775db50d867fcd7bdb975d15d7d1180cb7
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-08-23 12:05:56 +0100

    [task 0]: attempt to fix segfault

--- _printf.c
+++ _printf.c
@@ -16,19 +16,19 @@ int _printf(const char *format, ...)
 
 	char_count = 0;
 
-	if (format[0] == '\0')
-	{
-		return (0);
-	}
-
-	if ((format[0] == '%' && format[1] == '\0')
-			|| (format[0] == '%' && format[1] == ' ' && format[2] == '\0'))
-	{
-		return (-1);
-	}
-
 	if (format != NULL)
 	{
+
+		if (format[0] == '\0')
+		{
+			return (0);
+		}
+
+		if ((format[0] == '%' && format[1] == '\0')
+				|| (format[0] == '%' && format[1] == ' ' && format[2] == '\0'))
+		{
+			return (-1);
+		}
 		va_start(arg_ptr, format);
 		char_count += scan_format(format, arg_ptr);
 		va_end(arg_ptr);
